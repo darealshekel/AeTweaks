7bc91060c19123f3d7090d24585325c91bdff930
Shekel <65634940+darealshekel@users.noreply.github.com>
2026-04-06 14:13:42 +0300

    Add Minecraft 1.21 build support

--- src/main/java/com/miningtrackeraddon/event/RenderHandler.java
+++ src/main/java/com/miningtrackeraddon/event/RenderHandler.java
@@ -6,18 +6,15 @@ import com.miningtrackeraddon.hud.MiningHudRenderer;
 import fi.dy.masa.malilib.interfaces.IRenderer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.Fog;
-import net.minecraft.client.render.Frustum;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.profiler.Profiler;
 import org.joml.Matrix4f;
 
 public class RenderHandler implements IRenderer
 {
     @Override
-    public void onRenderGameOverlayPostAdvanced(DrawContext drawContext, float partialTicks, Profiler profiler, MinecraftClient mc)
+    public void onRenderGameOverlayPost(DrawContext drawContext)
     {
+        MinecraftClient mc = MinecraftClient.getInstance();
         if (FeatureToggle.TWEAK_MINING_TRACKER.getBooleanValue())
         {
             MiningHudRenderer.render(drawContext, mc);
@@ -30,7 +27,7 @@ public class RenderHandler implements IRenderer
     }
 
     @Override
-    public void onRenderWorldLastAdvanced(Matrix4f posMatrix, Matrix4f projMatrix, Frustum frustum, Camera camera, Fog fog, Profiler profiler)
+    public void onRenderWorldLast(Matrix4f posMatrix, Matrix4f projMatrix)
     {
     }
 }
